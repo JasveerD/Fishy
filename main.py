8b9bffb12d0f328cc38572eb2ac76b41c355acd7
Jasveer_D <jasveerdhillon05@gmail.com>
2025-01-22 14:36:31 -0500

    Implement the Lexer for Fishy and test it in main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from lexer import *
+
+if (__name__ == "__main__"):
+
+    source =  "IF+-123 foo*THEN/"
+    lexer = Lexer(source)
+
+    token = lexer.getToken()
+    while token.kind != TokenType.EOF:
+        print(token.kind)
+        token = lexer.getToken()
